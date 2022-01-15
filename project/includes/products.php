743b382c56fcd54145bf9d2ce9b3edca1d6f9dc7
PimMiii <pim.milt@gmail.com>
2022-01-15 18:43:59 +0100

    pattern matching products.php id

--- project/includes/products.php
+++ project/includes/products.php
@@ -1,47 +1,53 @@
 <?php
 require_once 'database.php';
-/** @var mysqli $db*/
+/** @var mysqli $db */
 
-function getAllProducts($db, string $page, int $user_role = 0){
-    //returns all entries from the `products` table in the database.
-    if($user_role == 1 && $page == 'admin') {
+function getAllProducts($db, string $page, int $user_role = 0)
+{
+    //returns all entries from the `products` table in the database
+    if ($user_role == 1 && $page == 'admin') {
         $query = "SELECT * FROM `products`";
-    }
-    elseif ($user_role == 0 && $page == 'admin'){
+    } elseif ($user_role == 0 && $page == 'admin') {
         exit();
-    }
-    else {
+    } else {
         $query = "SELECT * FROM `products` WHERE `visible` = 1";
     }
     $result = mysqli_query($db, $query)
     or die('DB ERROR: ' . mysqli_error($db) . " with query: " . $query);
-    if(mysqli_num_rows($result) != 0) {
+    if (mysqli_num_rows($result) != 0) {
         $products = [];
-        while($product = mysqli_fetch_assoc($result)) {
+        while ($product = mysqli_fetch_assoc($result)) {
             $products[] = $product;
         }
         return $products;
-    }
-    else {
+    } else {
         $products = [];
         return $products;
     }
 }
-function getProduct($db, int $id, int $user_role = 0) {
+
+function getProduct($db, string $id, int $user_role = 0)
+{
     // returns array with errors and the product requested through given id
-    $errors= [];
-    $product= [];
-    $query = "SELECT * FROM `products` WHERE `id`= '$id'";
-    $result = mysqli_query($db, $query)
-    or die('DB ERROR: ' . mysqli_error($db) . " with query: " . $query);
-    if(mysqli_num_rows($result) != 1) {
-        $errors['product'] = "Sorry, er is iets fout gegaan. Dit product kan niet worden gevonden.";
-    }
-    else{
-        $product = mysqli_fetch_assoc($result);
-        if($product['visible'] == 0 && $user_role != 1) {
+    $errors = [];
+    $product = [];
+    // check if id is only numbers [0-9]
+    if (!preg_match("/[^0-9]/", $id)) {
+        $query = "SELECT * FROM `products` WHERE `id`= '$id'";
+        $result = mysqli_query($db, $query)
+        or die('DB ERROR: ' . mysqli_error($db) . " with query: " . $query);
+        //check if only a single product was returned
+        if (mysqli_num_rows($result) != 1) {
             $errors['product'] = "Sorry, er is iets fout gegaan. Dit product kan niet worden gevonden.";
+        } else {
+            $product = mysqli_fetch_assoc($result);
+            //check if user has permissions to view this product
+            if ($product['visible'] == 0 && $user_role != 1) {
+                $errors['product'] = "Sorry, er is iets fout gegaan. Dit product kan niet worden gevonden.";
+            }
         }
+    } else {
+        $errors['product'] = "Sorry, er is iets fout gegaan. Dit product kan niet worden gevonden.";
     }
 
     return array('errors' => $errors, 'product' => $product);
